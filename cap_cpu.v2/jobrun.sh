1a8773bd3eb0bf5279807e99bbedb576f0ab561e
Daniele Forastieri <d.forastieri@almaviva.it>
2018-01-14 03:32:23 +0100

    deleted:    cap_cpu.v2/Dockerfile 	deleted:    cap_cpu.v2/KEYS 	deleted:    cap_cpu.v2/README.md 	deleted:    cap_cpu.v2/cronfile 	deleted:    cap_cpu.v2/docker-entrypoint.sh 	deleted:    cap_cpu.v2/flink-dependency-jar-1.0-SNAPSHOT.jar 	deleted:    cap_cpu.v2/flink-test-1.0-SNAPSHOT.jar 	deleted:    cap_cpu.v2/jobrun.sh

--- cap_cpu.v2/jobrun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-touch /var/log/esito 
-string=$(cat /var/log/esito)
-
-if [[ $string != *"finished"* ]]; then
-/opt/flink/bin/flink run -c it.almaviva.giotto.processing.WordCount /opt/flink/lib/flink-test-1.0-SNAPSHOT.jar > /var/log/esito
-fi
