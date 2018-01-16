c839209c2da25bf6c0e2f14b742737da075aef0a
Magopancione <sonoilmagopancione@gmail.com>
2018-01-16 13:03:11 +0100

    Update jobrun.sh

--- jobrun.sh
+++ jobrun.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/opt/flink/bin/flink run -c it.almaviva.giotto.processing.WordCount /opt/flink/lib/flink-test-1.0-SNAPSHOT.jar
+sleep 160;  nohup /opt/flink/bin/flink run -c it.almaviva.giotto.processing.WordCount /opt/flink/lib/flink-test-1.0-SNAPSHOT.jar &
+exit 0
